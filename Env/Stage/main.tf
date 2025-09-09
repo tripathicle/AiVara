a24938d2a255b24ba28c61357497a05fa6032c5c
tripathicle <tripathicle@gmail.com>
2025-09-10 00:44:42 +0530

    refactor(terraform): reorganize module structure and improve formatting

--- Env/Stage/main.tf
+++ Env/Stage/main.tf
@@ -4,3 +4,24 @@ module "AiVara-Rg" {
   location = var.location
 
 }
+
+module "AiVara-Vnet"{
+   depends_on = [ module.AiVara-Rg ]
+   source = "../../Modules/AiVara_Vnet"
+   
+   Vnet-name = var.Vnet-name
+    location = var.location
+    resource_group_name = var.name
+    address_space = var.address_space
+    dns_servers = var.dns_servers
+
+}
+
+module "AiVara-Subnet"{
+  depends_on = [ module.AiVara-Vnet ]
+  source = "../../Modules/AiVara_SubNet"
+  Subnet-name = var.Subnet-name
+  resource_group_name = var.name
+  Vnet-name = var.Vnet-name
+  address_prefixes = var.address_prefixes
+}
--- Env/Stage/var.tf
+++ Env/Stage/var.tf
@@ -8,3 +8,52 @@ variable "location" {
   type        = string
 
 }
+
+variable "Stg-name" {
+  description = "The name of the Storage Account."
+  type        = string
+
+}
+
+variable "account-tier" {
+  description = "The performance tier of the Storage Account. Valid options are 'Standard' and 'Premium'."
+  type        = string
+  default     = "Standard"
+}
+variable "account-replication-type" {
+  description = "The replication type of the Storage Account. Valid options are 'LRS', 'GRS', 'RAGRS', 'ZRS', and 'GZRS'."
+  type        = string
+  default     = "LRS"
+}
+
+variable "Vnet-name" {
+    description = "value for Vnet name"
+    type = string
+  
+}
+
+variable "address_space" {
+    description = "The address space that is used by the Virtual Network."
+    type        = list(string)
+}
+
+variable "dns_servers" {
+    description = "A list of DNS servers IP addresses."
+    type        = list(string)
+   
+  
+}
+
+
+variable "Subnet-name" {
+    description = "value for Subnet name"
+    type = string
+  
+}
+
+variable "address_prefixes" {
+    description = "The address prefixes to use for the subnet."
+    type        = list(string)
+   
+  
+}
