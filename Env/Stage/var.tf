c86411fe580226ddb0cc73f345eb589cfea7901d
Shubham Tripathi <tripathicle@gmail.com>
2025-09-09 16:56:06 +0530

    Feat/add storage module (#5)
    * refactor(variables): improve variable naming and descriptions for clarity
    
    * feat: add storage account module

--- Env/Stage/var.tf
+++ Env/Stage/var.tf
@@ -8,3 +8,20 @@ variable "location" {
   type        = string
 
 }
+
+variable "Stg-name" {
+  description = "The name of the Storage Account."
+  type        = string
+
+}
+
+variable "account-tier" {
+  description = "The performance tier of the Storage Account. Valid options are 'Standard' and 'Premium'."
+  type        = string
+  default     = "Standard"
+}
+variable "account-replication-type" {
+  description = "The replication type of the Storage Account. Valid options are 'LRS', 'GRS', 'RAGRS', 'ZRS', and 'GZRS'."
+  type        = string
+  default     = "LRS"
+}
