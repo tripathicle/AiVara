340a3d6e0f580728043ffba09417dddf9ac5d307
tripathicle <tripathicle@gmail.com>
2025-09-09 20:06:08 +0530

    feat: add virtual network module with outputs and variables

---
+++ Modules/AiVara_Vnet/outputs.tf
@@ -0,0 +1,26 @@
+output "name" {
+  value = azurerm_virtual_network.AiVara-Vnet-block.name
+
+}
+output "id" {
+  value = azurerm_virtual_network.AiVara-Vnet-block.id
+
+}
+output "location" {
+  value = azurerm_virtual_network.AiVara-Vnet-block.location
+
+}
+
+output "resource_group_name" {
+  value = azurerm_virtual_network.AiVara-Vnet-block.resource_group_name
+
+}
+output "address_space" {
+  value = azurerm_virtual_network.AiVara-Vnet-block.address_space
+
+}
+output "dns_servers" {
+  value = azurerm_virtual_network.AiVara-Vnet-block.dns_servers
+
+}
+
