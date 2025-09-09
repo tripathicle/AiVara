1c0ce6e235a91adb7b4cfa50c395cf8b984eda36
tripathicle <tripathicle@gmail.com>
2025-09-09 16:53:29 +0530

    feat: add storage account module

--- Env/Stage/main.tf
+++ Env/Stage/main.tf
@@ -4,3 +4,16 @@ module "AiVara-Rg" {
   location = var.location
 
 }
+
+module "AiVara-Storage-Acoount" {
+  depends_on = [module.AiVara-Rg]
+  source     = "../../Modules/AiVara_Storage_Acoount"
+  Stg-name   = var.Stg-name
+  location   = var.location
+  #rg-name = var.name
+
+  rg-name = module.AiVara-Rg.RG-Name-output
+
+  account-tier             = var.account-tier
+  account-replication-type = var.account-replication-type
+}
